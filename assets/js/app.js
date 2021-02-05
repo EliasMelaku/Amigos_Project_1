14a9326bfccb8967f18dbdc5f93b2f03ae77816d
Elias Melaku <se.Elias.Melaku@gmail.com>
2021-02-05 14:39:48 +0300

    basic structure configured, login page started

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+var username = document.querySelector(".username")
+
+username.addEventListener("blur", checkInput);
+
+
+function checkInput(){
+    var value  = username.value
+    if (value !== ""){
+        var userRegex = /^[A-Za-z0-9_-]{3,16}$/
+        var userResult = userRegex.test(value)
+        if(userResult == false)
+        {
+          document.querySelector(".username").style.boxShadow = "0 0 5px 0.2px #CA0B00"
+          document.querySelector(".username").style.border = "none"
+          console.log("Something is up");
+        }
+        else{
+          document.querySelector(".username").style.boxShadow = "0 0 5px 0.2px #4BB543"
+          document.querySelector(".username").style.border = "none"
+
+        }
+    }
+    else{
+        document.querySelector(".username").style.boxShadow = "0 0 5px 0.2px #CA0B00"
+        document.querySelector(".username").style.border = "none"
+        console.log("Something is up");
+
+    }
+}
