8b460e13ad17188d783b2ddfcad5590ceea135ca
Kaleb Besufikad <se.kaleb.besufikad@gmail.com>
2021-03-01 12:27:46 +0300

    detailed commits can be seen in /Kaleb-B branch

--- assets/js/displayItems.js
+++ assets/js/displayItems.js
@@ -15,61 +15,62 @@ var Email = document.querySelector(".email");
 // var edit = document.querySelector("approveEdit");
 
 document.addEventListener("DOMContentLoaded", () => {
-  displayUsers();
+    displayUsers();
 
-  let usersDBVanilla = indexedDB.open("users", 1);
+    let usersDBVanilla = indexedDB.open("users", 2);
 
-  // if there's an error
-  usersDBVanilla.onerror = function () {
-    console.log("There was an error");
-  };
-  // if everything is fine, assign the result to the instance
-  usersDBVanilla.onsuccess = function () {
-    // console.log('Database Ready');
+    // if there's an error
+    usersDBVanilla.onerror = function() {
+        console.log("There was an error");
+    };
+    // if everything is fine, assign the result to the instance
+    usersDBVanilla.onsuccess = function() {
+        // console.log('Database Ready');
 
-    // save the result
-    DB = usersDBVanilla.result;
-  };
+        // save the result
+        DB = usersDBVanilla.result;
+    };
 
-  editForm.addEventListener("submit", updateUserCredentials);
+    editForm.addEventListener("submit", updateUserCredentials);
 
-  function displayUsers() {
-    let usersTable = new Localbase("users");
+    function displayUsers() {
+        let usersTable = new Localbase("users");
 
-    usersTable
-      .collection("users")
-      .doc({ id: currentUserName })
-      .get()
-      .then((currentUser) => {
-        Username.value = currentUser.username;
-        Password.value = currentUser.password;
-        Email.value = currentUser.email;
-      });
-  }
-  function updateUserCredentials(e) {
-    e.preventDefault();
-    let usersTable = new Localbase("users");
+        usersTable
+            .collection("users")
+            .doc({ id: currentUserName })
+            .get()
+            .then((currentUser) => {
+                Username.value = currentUser.username;
+                Password.value = currentUser.password;
+                Email.value = currentUser.email;
+            });
+    }
 
-    usersTable
-      .collection("users")
-      .doc({ id: 1 })
-      .get()
-      .then((currentUser) => {
-        var transaction = DB.transaction(["users"], "readwrite");
-        var objectStore = transaction.objectStore("users");
+    function updateUserCredentials(e) {
+        e.preventDefault();
+        let usersTable = new Localbase("users");
 
-        let newValue = {
-          username: Username.value,
-          password: Password.value,
-          email: Email.value,
-          id: currentUser.id,
-        };
+        usersTable
+            .collection("users")
+            .doc({ id: 1 })
+            .get()
+            .then((currentUser) => {
+                var transaction = DB.transaction(["users"], "readwrite");
+                var objectStore = transaction.objectStore("users");
 
-        objectStore.put(newValue);
+                let newValue = {
+                    username: Username.value,
+                    password: Password.value,
+                    email: Email.value,
+                    id: currentUser.id,
+                };
 
-        currentUserName = newValue.username;
-        window.alert("Account credentials edited");
-        displayUsers();
-      });
-  }
+                objectStore.put(newValue);
+
+                currentUserName = newValue.username;
+                window.alert("Account credentials edited");
+                displayUsers();
+            });
+    }
 });
--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -6,24 +6,27 @@ const activeAuctions = document.querySelector(".activeAuctions");
 const myAuctions = document.querySelector(".myAuctions");
 const announcements = document.querySelector(".announcements");
 const profile = document.querySelector(".profile");
-
-newListing.addEventListener("click", function () {
-  window.location.href = `new_tender.html?username=${currentUser}`;
+const logout = document.querySelector(".logout");
+newListing.addEventListener("click", function() {
+    window.location.href = `new_tender.html?username=${currentUser}`;
 });
-activeAuctions.addEventListener("click", function () {
-  window.location.href = `auctions.html?username=${currentUser}`;
+activeAuctions.addEventListener("click", function() {
+    window.location.href = `auctions.html?username=${currentUser}`;
 });
-// newListing.addEventListener("click", function () {
-//   window.location.href = `myAuctions.html?username=${currentUser}`;
-// });
+myAuctions.addEventListener("click", function() {
+    window.location.href = `myAuctions.html?username=${currentUser}`;
+});
+logout.addEventListener("click", function() {
+    window.location.href = `signIn.html`;
+});
 // newListing.addEventListener("click", function () {
 //   window.location.href = `announcements.html?username=${currentUser}`;
 // });
 
-homepage.addEventListener("click", function () {
-  window.location.href = `index.html?username=${currentUser}`;
+homepage.addEventListener("click", function() {
+    window.location.href = `index.html?username=${currentUser}`;
 });
 
-profile.addEventListener("click", function () {
-  window.location.href = `profile.html?username=${currentUser}`;
+profile.addEventListener("click", function() {
+    window.location.href = `profile.html?username=${currentUser}`;
 });
